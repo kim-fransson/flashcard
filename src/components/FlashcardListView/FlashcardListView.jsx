634dd03f120bc23e7b8efb710e3343674fd0772d
Kim Fransson <kim.fransson.22@gmail.com>
2025-12-08 15:57:55 +0100

    Prisma setup

--- src/components/FlashcardListView/FlashcardListView.jsx
+++ src/components/FlashcardListView/FlashcardListView.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { listFlashcards } from "./helpers";
 
 async function FlashcardListView() {
+  const flashcards = await listFlashcards();
+
+  if (flashcards.length === 0) {
+    return (
+      <div>
+        <h3>No cards yet</h3>
+        <p>
+          Add your first card using the form above and it will show up
+          here.
+        </p>
+      </div>
+    );
+  }
   return <div></div>;
 }
 
--- src/components/FlashcardListView/helpers.js
+++ src/components/FlashcardListView/helpers.js
@@ -1,1 +1,12 @@
-export function listFlashcards() {}
+import prisma from "@/lib/prisma";
+
+export async function listFlashcards(cursorId = 0) {
+  const flashcards = await prisma.flashcard.findMany({
+    take: 12,
+    cursor: {
+      id: cursorId,
+    },
+  });
+
+  return flashcards;
+}
